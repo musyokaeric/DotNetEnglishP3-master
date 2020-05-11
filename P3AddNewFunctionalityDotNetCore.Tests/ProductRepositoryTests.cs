b9f030037be46fba63be72903ca1df649e105abf
musyokaeric <62944492+musyokaeric@users.noreply.github.com>
2020-05-11 16:10:10 +0300

    Product Servie And Reopsitory Tests

---
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductRepositoryTests.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Moq;
+using P3AddNewFunctionalityDotNetCore.Models.Entities;
+using P3AddNewFunctionalityDotNetCore.Models.Repositories;
+using P3AddNewFunctionalityDotNetCore.Models.Services;
+using System.Threading.Tasks;
+using Xunit;
+using P3AddNewFunctionalityDotNetCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq;
+
+namespace P3AddNewFunctionalityDotNetCore.Tests
+{
+    public class ProductRepositoryTests
+    {
+        /// <summary>
+        /// INTEGRATION TEST DB SETUP
+        /// </summary>
+        /// 
+
+        private P3Referential _context;
+
+        public ProductRepositoryTests()
+        {
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer("Server=.\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+            _context = new P3Referential(options);
+        }
+
+        /// <summary>
+        /// IN-MEMORY TEST SEUP
+        /// </summary>
+        /// 
+
+        private static List<Product> _testProductList => GenerateProductData();
+
+        private static List<Product> GenerateProductData()
+        {
+            List<Product> testProductsList = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Echo Dot",
+                    Description = "(2nd Generation) - Black",
+                    Quantity = 10,
+                    Price = 92.50
+                },
+
+                new Product
+                {
+                    Name = "Anker 3ft / 0.9m Nylon Braided",
+                    Description = "Tangle-Free Micro USB Cable",
+                    Quantity = 20,
+                    Price = 9.99
+                },
+
+                new Product
+                {
+                    Name = "JVC HAFX8R Headphone",
+                    Description = "Riptidz, In-Ear",
+                    Quantity = 30,
+                    Price = 69.99
+                },
+
+                new Product
+                {
+                    Name = "VTech CS6114 DECT 6.0",
+                    Description = "Cordless Phone",
+                    Quantity = 40,
+                    Price = 32.50
+                },
+
+                new Product
+                {
+                    Name = "NOKIA OEM BL-5J",
+                    Description = "Cell Phone",
+                    Quantity = 50,
+                    Price = 895.00
+                }
+            };
+            return testProductsList;
+        }
+
+        private DbContextOptions<P3Referential> TestDBContextOptionsBuilder()
+        {
+            return new DbContextOptionsBuilder<P3Referential>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        private void SeedTestDb(DbContextOptions<P3Referential> options)
+        {
+            using (var context = new P3Referential(options))
+            {
+                foreach (var item in _testProductList)
+                {
+                    context.Product.Add(item);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// PRODUCT REPOSITORY TESTS
+        /// </summary>
+        /// 
+
+        [Theory]
+        [InlineData(1, "(2nd Generation) - Black")]
+        [InlineData(2, "Tangle-Free Micro USB Cable")]
+        [InlineData(3, "Riptidz, In-Ear")]
+        public async Task TestGetProductIntegrationTestParametizedTestData(int id, string description)
+        {
+            // Arrange
+            var productRepository = new ProductRepository(_context);
+
+            //Act
+            var result = await productRepository.GetProduct(id);
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.IsType<Product>(result);
+            Assert.Equal(description, result.Description);
+
+            //worst case, id that does not exist = 6, -2
+        }
+
+        [Fact]
+        public async Task TestGetProductIntegrationTest()
+        {
+            //Arrange
+            var productRepository = new ProductRepository(_context);
+
+            //Act
+            var result = await productRepository.GetProduct();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.IsType<List<Product>>(result);
+            Assert.Equal(5, result.Count);
+        }
+
+        [Fact]
+        public void TestGetAllProductsIntegrationTest()
+        {
+            //Arrange
+            var productRepository = new ProductRepository(_context);
+
+            //Act
+            var result = productRepository.GetAllProducts();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.IsType<List<Product>>(result);
+            Assert.Equal(5, result.Count());
+        }
+
+        [Theory]
+        [InlineData(1, 5)]
+        [InlineData(2, 10)]
+        [InlineData(3, 15)]
+        [InlineData(4, 20)]
+        public void TestUpdateProductStocksInMemoryTestParametizedTestData(int id, int quantityToUpdate)
+        {
+            // Arrange
+            var product = new Product
+            {
+                Id = id,
+                Name = "name",
+                Description = "description",
+                Price = 12.95,
+                Quantity = 40
+            };
+            var options = TestDBContextOptionsBuilder();
+            using (var context = new P3Referential(options))
+            {
+                context.Product.Add(product);
+                context.SaveChanges();
+
+                var productRepository = new ProductRepository(context);
+                var quantity = product.Quantity - quantityToUpdate;
+
+                //Act
+                productRepository.UpdateProductStocks(id, quantityToUpdate);
+
+                //Assert
+                var result = context.Product.FirstOrDefault(p => p.Id == id);
+
+                Assert.NotNull(result);
+                Assert.Equal(quantity, result.Quantity);
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worst case = putting the wrong id
+            //worst case = entering a quantityToRemove to be of a higher quantity
+            //worst case = entering a nwgative quantityToRemove
+        }
+
+        [Fact]
+        public void TestSaveProductInMemoryTest()
+        {
+            // Arrange
+            var product = new Product
+            {
+                Id = 1,
+                Name = "one",
+                Description = "oneDescription",
+                Price = 12.05,
+                Quantity = 15
+            };
+
+            var options = TestDBContextOptionsBuilder();
+            using (var context = new P3Referential(options))
+            {
+                var productRepository = new ProductRepository(context);
+
+                // Act
+                productRepository.SaveProduct(product);
+
+                // Assert
+                var result = context.Product.ToList();
+
+                Assert.Single(result);
+                Assert.NotNull(result.First());
+                Assert.Equal("one", result.First().Name);
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worstcase = saving a null product
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void TestDeleteProductInMemoryTestParametizedTestData(int id)
+        {
+            // Arrange
+            var options = TestDBContextOptionsBuilder();
+            SeedTestDb(options);
+
+            using (var context = new P3Referential(options))
+            {
+                var productRepository = new ProductRepository(context);
+                var product = context.Product.FirstOrDefault(p => p.Id == id);
+
+                // Act
+                productRepository.DeleteProduct(id);
+
+                //Assert
+                var result = context.Product.FirstOrDefault(p => p.Id == id);
+
+                Assert.Null(result);
+                Assert.DoesNotContain(product, context.Product.ToList());
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worst case = enting the wrong id
+        }
+    }
+}
--- P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
+++ P3AddNewFunctionalityDotNetCore.Tests/ProductServiceTests.cs
@@ -4,84 +4,375 @@ using P3AddNewFunctionalityDotNetCore.Models.Repositories;
 using P3AddNewFunctionalityDotNetCore.Models.Services;
 using System.Threading.Tasks;
 using Xunit;
+using P3AddNewFunctionalityDotNetCore.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using P3AddNewFunctionalityDotNetCore.Models.ViewModels;
+using P3AddNewFunctionalityDotNetCore.Models;
 
 namespace P3AddNewFunctionalityDotNetCore.Tests
 {
     public class ProductServiceTests
     {
         /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
+        /// INTEGRATION TEST DB SETUP
         /// </summary>
-        [Fact]
-        public void ExampleMethod()
+        /// 
+
+        private P3Referential _context;
+
+        public ProductServiceTests()
         {
-            // Arrange
-
-            // Act
-
-
-            // Assert
-            Assert.Equal(1, 1);
+            var options = new DbContextOptionsBuilder<P3Referential>()
+                .UseSqlServer("Server=.\\SQLEXPRESS;Database=P3Referential-2f561d3b-493f-46fd-83c9-6e2643e7bd0a;Trusted_Connection=True;MultipleActiveResultSets=true")
+                .Options;
+            _context = new P3Referential(options);
         }
 
         /// <summary>
-        /// Take this test method as a template to write your test method.
-        /// A test method must check if a definite method does its job:
-        /// returns an expected value from a particular set of parameters
+        /// IN-MEMORY TEST SEUP
         /// </summary>
+        /// 
+
+        private static List<Product> _testProductList => GenerateProductData();
+
+        private static List<Product> GenerateProductData()
+        {
+            List<Product> testProductsList = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Echo Dot",
+                    Description = "(2nd Generation) - Black",
+                    Quantity = 10,
+                    Price = 92.50
+                },
+
+                new Product
+                {
+                    Name = "Anker 3ft / 0.9m Nylon Braided",
+                    Description = "Tangle-Free Micro USB Cable",
+                    Quantity = 20,
+                    Price = 9.99
+                },
+
+                new Product
+                {
+                    Name = "JVC HAFX8R Headphone",
+                    Description = "Riptidz, In-Ear",
+                    Quantity = 30,
+                    Price = 69.99
+                },
+
+                new Product
+                {
+                    Name = "VTech CS6114 DECT 6.0",
+                    Description = "Cordless Phone",
+                    Quantity = 40,
+                    Price = 32.50
+                },
+
+                new Product
+                {
+                    Name = "NOKIA OEM BL-5J",
+                    Description = "Cell Phone",
+                    Quantity = 50,
+                    Price = 895.00
+                }
+            };
+            return testProductsList;
+        }
+
+        private DbContextOptions<P3Referential> TestDBContextOptionsBuilder()
+        {
+            return new DbContextOptionsBuilder<P3Referential>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        }
+
+        private void SeedTestDb(DbContextOptions<P3Referential> options)
+        {
+            using (var context = new P3Referential(options))
+            {
+                foreach (var item in _testProductList)
+                {
+                    context.Product.Add(item);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        /// <summary>
+        /// PRODUCT SERVICE TESTS
+        /// </summary>
+
         [Fact]
-        public async Task TestGetProductByIdAsyncMockTest()
+        public void TestGetAllProductsViewModelMockTest()
         {
             // Arrange
-            Product product = new Product
-            {
-                Id = 1,
-                Description = "one",
-                Details = "onedetails",
+            var moqProductRepository = new Mock<IProductRepository>();
+            moqProductRepository.Setup(x => x.GetAllProducts()).Returns(GenerateProductData());
+            var productService = new ProductService(null, moqProductRepository.Object, null, null);
 
-            };
+            //Act
+            var result = productService.GetAllProductsViewModel();
 
-            var mockProductRepository = new Mock<IProductRepository>();
-            mockProductRepository.Setup(x => x.GetProduct(1)).Returns(Task.FromResult(product));
-            // Act
-            var productService = new ProductService(null, mockProductRepository.Object, null, null);
-            var result = await productService.GetProduct(1);
-            // Assert
-
+            //Assert
             Assert.NotNull(result);
-            Assert.Equal("one", result.Description);
+            Assert.NotEmpty(result);
+            Assert.IsType<List<ProductViewModel>>(result);
+            Assert.Equal(5, result.Count);
         }
 
-
-        //parameterized test
-        [Theory]
-        [InlineData(1, "one", "oneDetails")]
-        [InlineData(2, "two", "twoDetails")]
-        [InlineData(3, "three", "threeDetails")]
-        public async Task TestGetProductByIdAsyncMockTestParameterizedTestData(int id, string description, string details)
+        [Fact]
+        public void TestGetAllProductsIntegrationTest()
         {
+            // Arrange
+            var productRepository = new ProductRepository(_context);
+            var productService = new ProductService(null, productRepository, null, null);
 
+            // Act
+            var result = productService.GetAllProducts();
 
-            Product product = new Product
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.IsType<List<Product>>(result);
+            Assert.Equal(5, result.Count);
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void TestGetProductByIdViewModelMockTestParametizedTestData(int id)
+        {
+            // Aarrange
+            var products = new List<Product>
+            {
+                new Product{ Id = 1, Name = "one", Description = "oneDescription"},
+                new Product{ Id = 2, Name = "two", Description = "twoDescription"},
+                new Product{ Id = 3, Name = "three", Description = "threeDescription"},
+                new Product{ Id = 4, Name = "four", Description = "fourDescription"},
+                new Product{ Id = 5, Name = "five", Description = "fiveDescription"}
+            };
+            var name = products.FirstOrDefault(p => p.Id == id).Name;
+            var moqProductRepository = new Mock<IProductRepository>();
+            moqProductRepository.Setup(x => x.GetAllProducts()).Returns(products);
+            var productService = new ProductService(null, moqProductRepository.Object, null, null);
+
+            // Act
+            var result = productService.GetProductByIdViewModel(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<ProductViewModel>(result);
+            Assert.Equal(name, result.Name);
+
+            //worst case, entering the wrong id, -2, 10
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void TestGetProductByIdMockTestParametizedTestData(int id)
+        {
+            // Aarrange
+            var products = new List<Product>
+            {
+                new Product{ Id = 1, Name = "one", Description = "oneDescription"},
+                new Product{ Id = 2, Name = "two", Description = "twoDescription"},
+                new Product{ Id = 3, Name = "three", Description = "threeDescription"},
+                new Product{ Id = 4, Name = "four", Description = "fourDescription"},
+                new Product{ Id = 5, Name = "five", Description = "fiveDescription"}
+            };
+            var name = products.FirstOrDefault(p => p.Id == id).Name;
+            var moqProductRepository = new Mock<IProductRepository>();
+            moqProductRepository.Setup(x => x.GetAllProducts()).Returns(products);
+            var productService = new ProductService(null, moqProductRepository.Object, null, null);
+
+            // Act
+            var result = productService.GetProductById(id);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.IsType<Product>(result);
+            Assert.Equal(name, result.Name);
+
+            //worst case, entering the wrong id, -2, 10
+        }
+
+        [Theory]
+        [InlineData(1, "one", "oneDescription")]
+        [InlineData(2, "two", "twoDescription")]
+        [InlineData(3, "three", "threeDescription")]
+        [InlineData(4, "four", "fourDescription")]
+        [InlineData(5, "five", "fiveDescription")]
+        public async Task TestGetProductByIdAsyncMockTestParametizedTestData(int id, string name, string description)
+        {
+            // Aarrange
+            var product = new Product
             {
                 Id = id,
-                Description = description,
-                Details = details,
-
+                Name = name,
+                Description = description
             };
 
-            var mockProductRepository = new Mock<IProductRepository>();
-            mockProductRepository.Setup(x => x.GetProduct(id)).Returns(Task.FromResult(product));
-            // Act
-            var productService = new ProductService(null, mockProductRepository.Object, null, null);
-            var result = await productService.GetProduct(id);
-            // Assert
+            var moqProductRepository = new Mock<IProductRepository>();
+            moqProductRepository.Setup(x => x.GetProduct(id)).Returns(Task.FromResult(product));
+            var productService = new ProductService(null, moqProductRepository.Object, null, null);
 
+            //Act
+            var result = await productService.GetProduct(id);
+
+            //Assert
             Assert.NotNull(result);
-            Assert.Equal(description, result.Description);
+            Assert.IsType<Product>(result);
+            Assert.Equal(name, result.Name);
+
+            //Question: worst case by entering the wrong id?
         }
-        // TODO write test methods to ensure a correct coverage of all possibilities
+
+        [Fact]
+        public async Task TestGetAllProductsAsyncIntegrationTest()
+        {
+            //Arrange
+            var productRepository = new ProductRepository(_context);
+            var productService = new ProductService(null, productRepository, null, null);
+
+            //Act
+            var result = await productService.GetProduct();
+
+            //Assert
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+            Assert.Equal(5, result.Count);
+        }
+
+        [Theory]
+        [InlineData(5)]
+        [InlineData(10)]
+        [InlineData(15)]
+        public void TestUpdateProductQuantitiesInMemoryTestParametizedTestData(int quantity)
+        {
+            //Arrange
+            var product = new Product
+            {
+                Id = 1,
+                Name = "name",
+                Quantity = 20
+            };
+            var options = TestDBContextOptionsBuilder();
+            using (var context = new P3Referential(options))
+            {
+                context.Product.Add(product);
+                context.SaveChanges();
+
+                var cart = new Cart();
+                cart.AddItem(product, quantity);
+                int quantityLeft = product.Quantity - quantity;
+
+                var productRepository = new ProductRepository(context);
+                var productService = new ProductService(cart, productRepository, null, null);
+
+                //Act
+                productService.UpdateProductQuantities();
+                var result = context.Product.First().Quantity;
+
+                //Assert
+                Assert.Equal(quantityLeft, result);
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worst case, quantity to remove is larger than the product quantity, its a negative
+        }
+
+        [Fact]
+        public void TestCheckProductModelErrors()
+        {
+            //takes ProductViewModel as arguments - parametized
+            //uses IStringLocalizer - create your own
+            Assert.Equal(1, 2);
+        }
+
+        [Theory]
+        [InlineData(1, "one", "oneDescription", "oneDetails", "10.00", "50")]
+        [InlineData(2, "two", "twoDescription", "twoDetails", "20.00", "40")]
+        [InlineData(3, "three", "threeDescription", "threeDetails", "30.00", "30")]
+        [InlineData(4, "four", "fourDescription", "fourDetails", "40.00", "20")]
+        [InlineData(5, "five", "fiveDescription", "fiveDetails", "50.00", "10")]
+        public void TestSaveProductInMemoryTestParametizedTestData(int id, string name, string description, string details, string price, string stock)
+        {
+            //takes ProductViewModel as arguments - parametized
+            //writes to productRepository = inmemory
+            //Arrange
+            var productViewModel = new ProductViewModel
+            {
+                Id = id,
+                Name = name,
+                Description = description,
+                Details = details,
+                Price = price,
+                Stock = stock
+            };
+            var options = TestDBContextOptionsBuilder();
+            using (var context = new P3Referential(options))
+            {
+                var productRepository = new ProductRepository(context);
+                var productService = new ProductService(null, productRepository, null, null);
+
+                //Act
+                productService.SaveProduct(productViewModel);
+                var result = context.Product.ToList();
+
+                //Assert
+                Assert.Single(result);
+                Assert.Equal(name, result.First().Name);
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worst case, saving null product, saving product with null properties
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        [InlineData(3)]
+        [InlineData(4)]
+        [InlineData(5)]
+        public void TestDeleteProductInMemoryTestParametizedDataTest(int id)
+        {
+            //Arrange
+            var options = TestDBContextOptionsBuilder();
+            SeedTestDb(options);
+
+            using (var context = new P3Referential(options))
+            {
+                var productRepository = new ProductRepository(context);
+                var productService = new ProductService(null, productRepository, null, null);
+
+                //Act
+                productService.DeleteProduct(id);
+
+                //Assert
+                var result = context.Product.ToList();
+                Assert.Equal(4, result.Count);
+
+                context.Database.EnsureDeleted();
+            }
+
+            //worst case, entering the wrong id
+            //need to check on the cart scenario
+        }
     }
 }
